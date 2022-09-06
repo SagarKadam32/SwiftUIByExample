94cb9724b8ab798401e5e4ba196748b940100ca7
Sagar Kadam <>
2022-09-06 21:54:59 +0530

    Building a menu using List - Change-1

--- iDine/iDine/iDine/ContentView.swift
+++ iDine/iDine/iDine/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            List {
+                Text("Hello, World!")
+                Text("Hellow, World!")
+                Text("Hellow, World!")
+            }
+            .navigationTitle("Menu")
+        }
     }
 }
 
