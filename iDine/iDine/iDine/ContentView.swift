f12c57b239f4972cc980f9a6f760860bb2fbc73f
Sagar Kadam <>
2022-09-07 20:42:58 +0530

    Displaying a detail screen with NavigationLink - Change-1

--- iDine/iDine/iDine/ContentView.swift
+++ iDine/iDine/iDine/ContentView.swift
@@ -16,7 +16,9 @@ struct ContentView: View {
                 ForEach(menu) { section in
                     Section(header: Text(section.name)) {
                         ForEach(section.items) { item in
-                            ItemRow(item: item)
+                            NavigationLink(destination: Text(item.name)) {
+                                ItemRow(item: item)
+                            }
                         }
                     }
                 }
