08b6088f32830f9c1bd5bc7b5bd15ee2b000d85a
Sagar Kadam <>
2022-09-08 22:24:28 +0530

    How to format dates inside text views

--- iDine/iDine/iDine/MyLearnings/MyLearnings.swift
+++ iDine/iDine/iDine/MyLearnings/MyLearnings.swift
@@ -17,26 +17,35 @@ struct MyLearnings: View {
                     .fontWeight(.black)
                             
                 ) {
-                    
-                    NavigationLink(destination: Example_1_1()) {
-                        Text("1. How to create static labels with a Text view")
-                    }
-                    
-                    NavigationLink(destination: Example_1_2()) {
-                        Text("2. How to style text views with fonts, colors, line spacing, and more")
-                    }
-                    
-                    NavigationLink(destination: Example_1_3()) {
-                        Text("3. How to adjust text alignment using multilineTextAlignment()")
-                    }
-                    
-                    NavigationLink(destination: Example_1_4()) {
-                        Text("4. How to format text inside text views")
-                    }
-                    
-                    NavigationLink(destination: Example_1_5()) {
-                        Text("5. How to add spacing between letters in text")
-                    }
+                    Group {
+                            NavigationLink(destination: Example_1_1()) {
+                                Text("1. How to create static labels with a Text view")
+                            }
+                            
+                            NavigationLink(destination: Example_1_2()) {
+                                Text("2. How to style text views with fonts, colors, line spacing, and more")
+                            }
+                            
+                            NavigationLink(destination: Example_1_3()) {
+                                Text("3. How to adjust text alignment using multilineTextAlignment()")
+                            }
+                            
+                            NavigationLink(destination: Example_1_4()) {
+                                Text("4. How to format text inside text views")
+                            }
+                            
+                            NavigationLink(destination: Example_1_5()) {
+                                Text("5. How to add spacing between letters in text")
+                            }
+                        }
+                        Group {
+                            NavigationLink(destination: Example_1_6()) {
+                                Text("6. How to format dates inside text views")
+                            }
+                       
+                        
+                        }
+
                 }
             }
             .navigationTitle("My Learnings")
---
+++ iDine/iDine/iDine/MyLearnings/Section-1/Example_1_6.swift
@@ -0,0 +1,29 @@
+//
+//  Example_1_6.swift
+//  iDine
+//
+//  Created by Sagar Kadam on 08/09/22.
+//
+
+import SwiftUI
+
+struct Example_1_6: View {
+    var body: some View {
+        NavigationView {
+            VStack(spacing:10) {
+                Text(Date()...Date().addingTimeInterval(600))
+                Text(Date().addingTimeInterval(600), style: .date)
+                Text(Date().addingTimeInterval(600), style: .time)
+                Text(Date().addingTimeInterval(600), style: .relative)
+                Text(Date().addingTimeInterval(600), style: .timer)
+            }
+            .navigationTitle("6. How to format dates inside text views")
+            .navigationBarTitleDisplayMode(.inline)
+        }    }
+}
+
+struct Example_1_6_Previews: PreviewProvider {
+    static var previews: some View {
+        Example_1_6()
+    }
+}
