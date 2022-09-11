2259a7843988f10fcc5fa215df545eff0fbb1fc7
Sagar Kadam <>
2022-09-11 16:57:48 +0530

    Formatting alignment

--- iDine/iDine/iDine/MyLearnings/MyLearnings.swift
+++ iDine/iDine/iDine/MyLearnings/MyLearnings.swift
@@ -7,37 +7,48 @@
 
 import SwiftUI
 
+struct SectionHeader : View {
+    var sectionTitle = ""
+    var body: some View {
+        Text(sectionTitle)
+            .frame(maxWidth: .infinity, maxHeight: 35, alignment: .leading)
+            .padding()
+            .font(.title)
+            .background(.mint)
+    }
+}
+
 struct MyLearnings: View {
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack(spacing: 20) {
                     VStack(alignment: .leading, spacing: 10) {
-                        Text("Working with static text")
-                            .frame(maxWidth: .infinity, maxHeight: 35, alignment: .leading)
-                            .padding()
-                            .font(.title)
-                            .background(.mint)
+                        SectionHeader(sectionTitle: "Working with static text")
                         Example_1_Main()
                             .frame(maxWidth: .infinity,alignment: .leading)
                     }
                     Divider()
                     VStack(alignment: .leading, spacing: 10) {
-                        Text("View layout")
-                            .frame(maxWidth: .infinity, maxHeight: 35, alignment: .leading)
-                            .padding()
-                            .font(.title)
-                            .background(.mint)
+                        SectionHeader(sectionTitle: "View layout")
                         Example_2_Main()
                             .frame(maxWidth: .infinity,alignment: .leading)
                     }
                     Divider()
                     VStack(alignment: .leading, spacing: 10) {
-                        Text("Stacks, grids, scrollviews")
-                            .frame(maxWidth: .infinity, maxHeight: 35, alignment: .leading)
-                            .padding()
-                            .font(.title)
-                            .background(.mint)
+                        SectionHeader(sectionTitle: "Stacks, grids, scrollviews")
+                        Example_3_Main()
+                            .frame(maxWidth: .infinity,alignment: .leading)
+                    }
+                    Divider()
+                    VStack(alignment: .leading, spacing: 10) {
+                        SectionHeader(sectionTitle: "Stacks, grids, scrollviews")
+                        Example_3_Main()
+                            .frame(maxWidth: .infinity,alignment: .leading)
+                    }
+                    Divider()
+                    VStack(alignment: .leading, spacing: 10) {
+                        SectionHeader(sectionTitle: "User interface controls")
                         Example_3_Main()
                             .frame(maxWidth: .infinity,alignment: .leading)
                     }
