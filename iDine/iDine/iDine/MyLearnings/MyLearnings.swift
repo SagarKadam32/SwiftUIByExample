42277595360bf5d59ce4d598681821287752f2b7
Sagar Kadam <>
2022-09-11 17:05:05 +0530

    Formatting alignment

--- iDine/iDine/iDine/MyLearnings/MyLearnings.swift
+++ iDine/iDine/iDine/MyLearnings/MyLearnings.swift
@@ -9,12 +9,22 @@ import SwiftUI
 
 struct SectionHeader : View {
     var sectionTitle = ""
+    var sectionTitleDescription = ""
+
     var body: some View {
-        Text(sectionTitle)
-            .frame(maxWidth: .infinity, maxHeight: 35, alignment: .leading)
-            .padding()
-            .font(.title)
-            .background(.mint)
+        VStack(spacing:0) {
+            Text(sectionTitle)
+                .frame(maxWidth: .infinity, maxHeight: 35, alignment: .leading)
+                .padding()
+                .font(.title)
+                .background(.mint)
+            Text(sectionTitleDescription)
+                .frame(maxWidth: .infinity, minHeight: 15, maxHeight: 25, alignment: .leading)
+                .padding()
+                .font(.headline.bold())
+                .background(.yellow)
+        }
+
     }
 }
 
@@ -24,35 +34,30 @@ struct MyLearnings: View {
             ScrollView {
                 VStack(spacing: 20) {
                     VStack(alignment: .leading, spacing: 10) {
-                        SectionHeader(sectionTitle: "Working with static text")
+                        SectionHeader(sectionTitle: "Working with static text", sectionTitleDescription: "Laying out text neatly")
                         Example_1_Main()
                             .frame(maxWidth: .infinity,alignment: .leading)
                     }
                     Divider()
                     VStack(alignment: .leading, spacing: 10) {
-                        SectionHeader(sectionTitle: "View layout")
+                        SectionHeader(sectionTitle: "View layout", sectionTitleDescription: "Layout sizes, priorities, and spacing")
                         Example_2_Main()
                             .frame(maxWidth: .infinity,alignment: .leading)
                     }
                     Divider()
                     VStack(alignment: .leading, spacing: 10) {
-                        SectionHeader(sectionTitle: "Stacks, grids, scrollviews")
+                        SectionHeader(sectionTitle: "Stacks, grids, scrollviews", sectionTitleDescription: "Position views in a structured way")
                         Example_3_Main()
                             .frame(maxWidth: .infinity,alignment: .leading)
                     }
                     Divider()
                     VStack(alignment: .leading, spacing: 10) {
-                        SectionHeader(sectionTitle: "Stacks, grids, scrollviews")
+                        SectionHeader(sectionTitle: "User interface controls", sectionTitleDescription: "Respond to interaction and control your program state")
                         Example_3_Main()
                             .frame(maxWidth: .infinity,alignment: .leading)
                     }
                     Divider()
-                    VStack(alignment: .leading, spacing: 10) {
-                        SectionHeader(sectionTitle: "User interface controls")
-                        Example_3_Main()
-                            .frame(maxWidth: .infinity,alignment: .leading)
-                    }
-                    Divider()
+
                 }
                 .padding()
             }
