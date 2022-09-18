93c7bd2e10d2f82f6801a740abfb988cd49f2728
Sagar Kadam <>
2022-09-18 13:56:15 +0530

    8.9 - How to adjust List row separator visibility and color

--- iDine/iDine/iDine/MyLearnings/Section-8/Example_8_9.swift
+++ iDine/iDine/iDine/MyLearnings/Section-8/Example_8_9.swift
@@ -15,22 +15,42 @@ struct User: Identifiable {
 struct Example_8_9: View {
     let users = [User(),User(),User(),User(),User()]
     var body: some View {
-        VStack {
+  
+            VStack {
 
-            List(users) { user in
-                HStack {
-                    Image(systemName: "person.circle")
-                        .resizable()
-                        .frame(width: 40, height: 40)
-                    Text(user.userName)
+                List(users) { user in
+                    HStack {
+                        Image(systemName: "person.circle")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                        Text(user.userName)
+                    }
                 }
+                
+                List {
+                    ForEach(1..<100) { index in
+                        Text("Row \(index)")
+                            .listRowSeparator(.hidden)
+                    }
+                }
+                
+                List {
+                    ForEach(1..<100) { index in
+                        Text("Row \(index)")
+                            .listRowSeparatorTint(.red)
+                    }
+                }
+             
+                
+                MoreDetailsView(linkURL: "https://www.hackingwithswift.com/quick-start/swiftui/how-to-use-implicit-stacking", title: "Implicit stacking")
+                Divider()
+                MoreDetailsView(linkURL: "https://www.hackingwithswift.com/quick-start/swiftui/how-to-adjust-list-row-separator-visibility-and-color", title: "List row separator")
             }
-         
-            
-            MoreDetailsView(linkURL: "https://www.hackingwithswift.com/quick-start/swiftui/how-to-use-implicit-stacking", title: "Implicit stacking")
-        }
-       
-        .padding()
+        
+           
+            .padding()
+
+
     }
 }
 
