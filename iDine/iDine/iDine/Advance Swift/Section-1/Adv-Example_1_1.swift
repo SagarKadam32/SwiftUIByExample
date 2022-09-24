f550bbeb9214df14b5231d9f5ef8a6547437360f
Sagar Kadam <>
2022-09-25 00:06:16 +0530

    Code Update : Addition of the generic type for the Model

--- iDine/iDine/iDine/Advance Swift/Section-1/Adv-Example_1_1.swift
+++ iDine/iDine/iDine/Advance Swift/Section-1/Adv-Example_1_1.swift
@@ -20,12 +20,26 @@ struct BoolModel {
         BoolModel(info: nil)
     }
 }
+
+struct GenericModel<CustomType> {
+    let info: CustomType?
+    func removeInfo() -> GenericModel {
+        GenericModel(info: nil)
+    }
+}
 class GenericsViewModel: ObservableObject {
     @Published var stringModel = StringModel(info: "Hello World!")
     @Published var boolModel = BoolModel(info: true)
+    
+    @Published var genericStringModel = GenericModel(info: "Generic Hello Wordl!")
+    @Published var genericBoolModel = GenericModel(info: true)
+    
     func removeData() {
         stringModel = stringModel.removeInfo()
         boolModel = boolModel.removeInfo()
+        
+        genericStringModel = genericStringModel.removeInfo()
+        genericBoolModel = genericBoolModel.removeInfo()
     }
 }
 
@@ -35,6 +49,9 @@ struct Adv_Example_1_1: View {
         VStack {
             Text(vm.stringModel.info ?? "No String Data")
             Text(vm.boolModel.info?.description ?? "No Bool Data")
+            
+            Text(vm.genericStringModel.info ?? "No Generic String Data")
+            Text(vm.genericBoolModel.info?.description ?? "No Generic Bool Data")
             
             Section {
                 MoreDetailsView(linkURL: "https://www.youtube.com/watch?v=rx3uRICZr5I&list=PLwvDm4Vfkdphc1LLLjCaEd87BEg07M97y&index=9", title: "Generics")
