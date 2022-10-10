91e3046861ca052cc4b6211b29b6c290947356e7
Sagar Kadam <>
2022-10-10 10:09:46 +0530

    Code Update : In this code what we are trying to do is that there is text field that we shall listen on value change. This is basically a listener to the TextField. We are using the map function of the text field where we check if the value of the text entered in the textfield is greater than 3 or not.
    The problem here is the we are using "ASSIGN" property to assign the value returned of map on the SELF object which is not a good practice as we can not make it weak self in the Assign,
    Instead to avoid this problem we shall be using the sink-receive with weak self to be potentially safe and secure.

--- iDine/iDine/iDine/Advance Swift/Section-1/SubscriberBootcamp.swift
+++ iDine/iDine/iDine/Advance Swift/Section-1/SubscriberBootcamp.swift
@@ -11,26 +11,43 @@ import Combine
 class SubscriberViewModel: ObservableObject {
     
     @Published var count: Int = 0
+    @Published var textField: String = ""
+    @Published var textIsValid: Bool = false
+    
     var timer : AnyCancellable?
     var cancellables = Set<AnyCancellable>()
     
     init() {
         setUpTimer()
+        addTextFieldSubscriber()
+    }
+    
+    func addTextFieldSubscriber() {
+        $textField
+            .map { (text) -> Bool in
+                if text.count > 3 {
+                    return true
+                }
+                return false
+            }
+            .assign(to: \.textIsValid, on: self)
+            .store(in: &cancellables)
     }
     
     func setUpTimer() {
         Timer
-            .publish(every: 0.01, on: .main, in: .common)
+            .publish(every: 1.0, on: .main, in: .common)
             .autoconnect()
             .sink { [weak self] _ in
                 guard let strongSelf = self else { return }
                 strongSelf.count += 1
                 
+                /*
                 if strongSelf.count >= 100 {
                     for item in strongSelf.cancellables {
                         item.cancel()
                     }
-                }
+                } */
             }
             .store(in: &cancellables)
     }
@@ -50,7 +67,14 @@ struct SubscriberBootcamp: View {
                 .font(.largeTitle)
                 .foregroundColor(.blue)
             
+            Text(vm.textIsValid.description)
             
+            TextField("Type something here...", text:  $vm.textField)
+                .padding(.leading)
+                .frame(height: 55)
+                .font(.headline)
+                .background(Color.gray)
+                .cornerRadius(10)
             
             Section {
                 MoreDetailsView(linkURL: "https://www.youtube.com/watch?v=Q-1EDHXUunI&list=PLwvDm4VfkdpiagxAXCT33Rkwnc5IVhTar", title: "Publishers and Subscribers in Combine")
