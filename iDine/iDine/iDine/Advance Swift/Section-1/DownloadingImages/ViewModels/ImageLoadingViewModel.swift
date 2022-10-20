0675e9817df57ae6883b0512b3caa8c04a742bd8
Sagar Kadam <>
2022-10-21 02:01:56 +0530

    Image loading from the internet

---
+++ iDine/iDine/iDine/Advance Swift/Section-1/DownloadingImages/ViewModels/ImageLoadingViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  ImageLoadingViewModel.swift
+//  iDine
+//
+//  Created by Sagar Kadam on 20/10/22.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class ImageLoadingViewModel: ObservableObject {
+    
+    @Published var image: UIImage? = nil
+    @Published var isLoading: Bool = false
+    
+    let urlString: String
+    var cancellable = Set<AnyCancellable>()
+    
+    init(url: String) {
+        urlString = url
+        downloadImage()
+    }
+    
+    func downloadImage() {
+        isLoading = true
+        guard let url = URL(string: urlString) else {
+            isLoading = false
+            return
+        }
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map { return UIImage(data: $0.data)}
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] (_) in
+                self?.isLoading = false
+            } receiveValue: { [weak self] (returnedImage) in
+                self?.image = returnedImage
+            }
+            .store(in: &cancellable)
+
+    }
+}
