8b56b3f0c2bdbd771d318750e1d9943b60462490
Sagar Kadam <>
2022-12-18 12:03:03 +0530

    How to use and ignore the Safe Area in SwiftUI | Bootcamp #17

--- iDine/iDine/iDine/Basics/Basics_5_SafeArea_Bootcamp.swift
+++ iDine/iDine/iDine/Basics/Basics_5_SafeArea_Bootcamp.swift
@@ -9,34 +9,28 @@ import SwiftUI
 
 struct Basics_5_SafeArea_Bootcamp: View {
     var body: some View {
-        ZStack {
-            
-            Color.blue
-                .edgesIgnoringSafeArea(.all)
-            
-            ScrollView {
-                VStack {
-                    Text("Title goes here")
-                        .font(.largeTitle)
-                        .frame(maxWidth: .infinity, alignment: .leading)
+        
+        ScrollView {
+            VStack {
+                Text("Title goes here")
+                    .font(.largeTitle)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                
+                ForEach(0..<10) { index in
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .fill(Color.white)
+                        .frame(height: 150)
+                        .shadow(radius: 10)
+                        .padding(20)
                     
-                    ForEach(0..<10) { index in
-                        RoundedRectangle(cornerRadius: 25.0)
-                            .fill(Color.white)
-                            .frame(height: 150)
-                            .shadow(radius: 10)
-                            .padding(20)
-                        
-                    }
                 }
             }
-            .padding()
-            
         }
-        
-
-
-        
+        .background(
+            Color.red
+                .ignoresSafeArea()
+        )
         /*
         ZStack {
             // background
