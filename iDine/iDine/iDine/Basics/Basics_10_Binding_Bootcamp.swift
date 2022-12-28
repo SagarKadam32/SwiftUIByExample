735dcede98fc55a6ad60b52cb23e6f4c0315eca5
Sagar Kadam <>
2022-12-28 18:59:43 +0530

    How to use @Binding property wrapper in SwiftUI | Bootcamp #22
    Created a new button for reset and reset the background colour.

--- iDine/iDine/iDine/Basics/Basics_10_Binding_Bootcamp.swift
+++ iDine/iDine/iDine/Basics/Basics_10_Binding_Bootcamp.swift
@@ -10,11 +10,26 @@ import SwiftUI
 struct Basics_10_Binding_Bootcamp: View {
     
     @State var backgroundColor: Color = .green
+    @State var title: String = "Title"
+    @State var buttonTitle1: String = "Reset"
+    @State var buttonTitle2: String = "Try Me !"
+
     var body: some View {
         ZStack {
             backgroundColor
                 .edgesIgnoringSafeArea(.all)
-            ButtonView(backgroundColor: $backgroundColor)
+            VStack {
+                Text(title)
+                ButtonView(backgroundColor: $backgroundColor, title: $title, buttonTitle: $buttonTitle1)
+                ButtonView(backgroundColor: $backgroundColor, title: $title, buttonTitle: $buttonTitle2)
+                
+                Section {
+                    MoreDetailsView(linkURL: "https://www.youtube.com/watch?v=btDMzB5x2Gs&list=PLwvDm4VfkdphqETTBf-DdjCoAvhai1QpO&index=23", title: "How to use @Binding property wrapper in SwiftUI | Bootcamp #22")
+                }
+                .foregroundColor(.white)
+
+            }
+            
         }
     }
 }
@@ -27,23 +42,30 @@ struct Basics_10_Binding_Bootcamp_Previews: PreviewProvider {
 
 struct ButtonView: View {
     @Binding var backgroundColor: Color
+    @Binding var title: String
+    @Binding var buttonTitle: String
+    @State var buttonColor: Color = .blue
+    
     var body: some View {
         VStack {
             Button(action: {
                 backgroundColor = .orange
+                buttonColor = .pink
+                title = "New Title !!!!!!!"
+                
+                if buttonTitle == "Reset" {
+                    backgroundColor = .green
+                    buttonColor = .blue
+                    title = "Old Title"
+                }
             }, label: {
-                Text("Button")
+                Text(buttonTitle)
                     .foregroundColor(.white)
                     .padding()
                     .padding(.horizontal)
-                    .background(.blue)
+                    .background(buttonColor)
                     .cornerRadius(10)
             })
-            
-            Section {
-                MoreDetailsView(linkURL: "https://www.youtube.com/watch?v=btDMzB5x2Gs&list=PLwvDm4VfkdphqETTBf-DdjCoAvhai1QpO&index=23", title: "How to use @Binding property wrapper in SwiftUI | Bootcamp #22")
-            }
-            .foregroundColor(.white)
         }
     }
 }
