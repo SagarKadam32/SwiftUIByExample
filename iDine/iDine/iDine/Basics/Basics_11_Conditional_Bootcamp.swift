cf111b841a7e1f632a0182cdddd8ca36b723b41a
Sagar Kadam <>
2022-12-28 20:01:10 +0530

    How to use if-else and conditional statements in SwiftUI | Bootcamp #23
    In this video we will learn how to use conditional logic in our SwiftUI applications. We will first learn how to create "if - else" statements and then learn more advanced operations, such as "if-not", "or" & "and" statements. This logic will help us create complex and dynamic views when we start building iOS applications!

--- iDine/iDine/iDine/Basics/Basics_11_Conditional_Bootcamp.swift
+++ iDine/iDine/iDine/Basics/Basics_11_Conditional_Bootcamp.swift
@@ -11,9 +11,15 @@ struct Basics_11_Conditional_Bootcamp: View {
     
     @State var showCircle: Bool = false
     @State var showRectangle: Bool = false
-    
+    @State var isLoading: Bool = true
+    @State var shouldHideLoadingView: Bool = false
     var body: some View {
         VStack(spacing: 20){
+            
+            Button("Click here to stop progressview & start..") {
+                isLoading.toggle()
+                shouldHideLoadingView.toggle()
+            }.opacity(shouldHideLoadingView ? 0 : 1)
             
             Button("Circle Button: \(showCircle.description)") {
                 showCircle.toggle()
@@ -22,21 +28,42 @@ struct Basics_11_Conditional_Bootcamp: View {
                 showRectangle.toggle()
             }
             
-            if showCircle {
-                Circle()
-                    .frame(width: 100, height: 100)
-            } else if showRectangle {
-                Rectangle()
-                    .frame(width: 100, height: 100)
+            if isLoading {
+                LoadingView(shouldHideLoadingView: $shouldHideLoadingView, isLoading: $isLoading)
+
+                ProgressView()
             } else {
-                RoundedRectangle(cornerRadius: 25)
-                    .frame(width: 200, height: 200)
+
+                
+                if showCircle {
+                    Circle()
+                        .frame(width: 100, height: 100)
+                }
+                
+                if showRectangle {
+                    Rectangle()
+                        .frame(width: 100, height: 100)
+                }
+                
+                
+                if showCircle && showRectangle {
+                    RoundedRectangle(cornerRadius: 25)
+                        .frame(width: 200, height: 200)
+                }else if(!showCircle && !showRectangle) {
+                    LoadingView(shouldHideLoadingView: $shouldHideLoadingView, isLoading: $isLoading)
+                        .onAppear(){
+                            shouldHideLoadingView = false
+                            isLoading = true
+                        }
+                }
             }
+            
+
             
             Spacer()
             
             Section {
-                MoreDetailsView(linkURL: "https://www.youtube.com/watch?v=su0KLQq0JM0&list=PLwvDm4VfkdphqETTBf-DdjCoAvhai1QpO&index=14", title: "How to use inits and enums in SwiftUI | Bootcamp #13")
+                MoreDetailsView(linkURL: "https://www.youtube.com/watch?v=W8sGT16WAkQ&list=PLwvDm4VfkdphqETTBf-DdjCoAvhai1QpO&index=24", title: "How to use if-else and conditional statements in SwiftUI | Bootcamp #23")
             }
         }
     }
@@ -45,5 +72,16 @@ struct Basics_11_Conditional_Bootcamp: View {
 struct Basics_11_Conditional_Bootcamp_Previews: PreviewProvider {
     static var previews: some View {
         Basics_11_Conditional_Bootcamp()
+    }
+}
+
+struct LoadingView: View {
+    @Binding var shouldHideLoadingView: Bool
+    @Binding var isLoading: Bool
+    var body: some View {
+        Button("Click here to stop progressview & start..") {
+            isLoading.toggle()
+            shouldHideLoadingView.toggle()
+        }.opacity(shouldHideLoadingView ? 0 : 1)
     }
 }
