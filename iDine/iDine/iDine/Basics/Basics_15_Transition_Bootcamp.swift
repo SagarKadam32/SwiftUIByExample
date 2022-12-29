9cc0c7210d586efc3723cae52c386e13aaee3e40
Sagar Kadam <>
2022-12-29 19:25:16 +0530

    How to use Transition in SwiftUI | Bootcamp #27

--- iDine/iDine/iDine/Basics/Basics_15_Transition_Bootcamp.swift
+++ iDine/iDine/iDine/Basics/Basics_15_Transition_Bootcamp.swift
@@ -20,13 +20,14 @@ struct Basics_15_Transition_Bootcamp: View {
                 }
                 Spacer()
                 
-                RoundedRectangle(cornerRadius: 30)
-                    .frame(height: UIScreen.main.bounds.height * 0.5)
-                    .opacity(showView ? 1.0 : 0.0)
-                    .animation(.easeInOut, value: 10.0)
-                
+                if showView {
+                    RoundedRectangle(cornerRadius: 30)
+                        .frame(height: UIScreen.main.bounds.height * 0.5)
+                        .transition(.slide)
+                        .animation(.easeInOut, value: 10.0)
+                }
                 Section {
-                    MoreDetailsView(linkURL: "https://www.youtube.com/watch?v=X6FAIa0nJoA&list=PLwvDm4VfkdphqETTBf-DdjCoAvhai1QpO&index=28", title: "How to use inits and enums in SwiftUI | Bootcamp #13")
+                    MoreDetailsView(linkURL: "https://www.youtube.com/watch?v=X6FAIa0nJoA&list=PLwvDm4VfkdphqETTBf-DdjCoAvhai1QpO&index=28", title: "How to use Transition in SwiftUI | Bootcamp #27")
                 }
                 
             }
