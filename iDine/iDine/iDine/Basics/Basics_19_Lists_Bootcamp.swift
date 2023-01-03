2c27694b93250d38768c46b8dfbd331bc1667cfc
SagarKadam32 <misc.sagarkadam@gmail.com>
2023-01-03 02:39:00 -0800

    Test Commit

---
+++ iDine/iDine/iDine/Basics/Basics_19_Lists_Bootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  Basics_19_Lists_Bootcamp.swift
+//  iDine
+//
+//  Created by Capgemini-DA073 on 1/3/23.
+//
+
+import SwiftUI
+
+struct Basics_19_Lists_Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Basics_19_Lists_Bootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        Basics_19_Lists_Bootcamp()
+    }
+}
