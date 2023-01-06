8182734b67855c883ad3e45a222ac6b820508569
SagarKadam32 <misc.sagarkadam@gmail.com>
2023-01-06 06:17:33 -0800

    How to show Alerts in SwiftUI app | Bootcamp #32 - Updated
    1. Added the another alert view and associated action items.

--- iDine/iDine/iDine/Basics/Basics_20_Alerts_Bootcamp.swift
+++ iDine/iDine/iDine/Basics/Basics_20_Alerts_Bootcamp.swift
@@ -10,12 +10,28 @@ import SwiftUI
 struct Basics_20_Alerts_Bootcamp: View {
     
     @State var showAlert: Bool = false
+    @State var backgroundColor: Color = .yellow
     var body: some View {
-        Button("Click Me!!") {
-            showAlert.toggle()
+        ZStack {
+            backgroundColor.edgesIgnoringSafeArea(.all)
+            Button("Click Me!!") {
+                showAlert.toggle()
+            }
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .alert(isPresented: $showAlert) {
+                Alert(
+                    title: Text("Alert Title"),
+                    message: Text("This is optional error message"),
+                    primaryButton: .destructive(Text("Delete"), action: {
+                        // Delete Action
+                        backgroundColor = .red
+                    }),
+                    secondaryButton: .cancel(Text("Reset"), action: {
+                        backgroundColor = .yellow
+                    }))
+                //Alert(title: Text("There was an error!"))
         }
-        .alert(isPresented: $showAlert) {
-            Alert(title: Text("There was an error!"))
         }
 
     }
