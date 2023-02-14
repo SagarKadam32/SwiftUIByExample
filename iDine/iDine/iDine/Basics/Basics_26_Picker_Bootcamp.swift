1ee2dab3e7d5acf94e83bd432918b569ee233136
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-02-14 12:15:35 +0530

    Update : Added the Wheel Picker Style

--- iDine/iDine/iDine/Basics/Basics_26_Picker_Bootcamp.swift
+++ iDine/iDine/iDine/Basics/Basics_26_Picker_Bootcamp.swift
@@ -22,11 +22,13 @@ struct Basics_26_Picker_Bootcamp: View {
                 selection: $mySelection,
                 label: Text("Picker"),
                    content: {
-                Text("1").tag("1")
-                Text("2").tag("2")
-                Text("3").tag("3")
-                Text("4").tag("4")
-                Text("5").tag("5")
+                       ForEach(18..<100, content: { number in
+                            Text("\(number)")
+                               .font(.headline)
+                               .foregroundColor(.red)
+                               .tag("\(number)")
+                       })
+                       .pickerStyle(WheelPickerStyle())
             })
         }
     }
