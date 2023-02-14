73a545c1ebbb8380548fd0405d616873c8a46fc6
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-02-14 12:36:54 +0530

    [Update] Picker - Menu Picker Style

--- iDine/iDine/iDine/Basics/Basics_26_Picker_Bootcamp.swift
+++ iDine/iDine/iDine/Basics/Basics_26_Picker_Bootcamp.swift
@@ -9,9 +9,42 @@ import SwiftUI
 
 struct Basics_26_Picker_Bootcamp: View {
     
-    @State var mySelection: String = "1"
+    @State var mySelection: String = "Most Recent"
+    let myFilterOptions: [String] = ["Most Recent", "Most Popular", "Most Liked"]
+    
     var body: some View {
         
+        /* MenuPickerStyle */
+        Picker(
+            selection: $mySelection,
+            label:
+                HStack {
+                    Text("Filter:")
+                    Text(mySelection)
+                }
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .padding(.horizontal)
+                .background(.blue)
+                .cornerRadius(10)
+                .shadow(color: .blue, radius: 10, x: 0, y: 10)
+            
+            ,
+            content: {
+                ForEach(myFilterOptions, id: \.self) { option in
+                    HStack {
+                        Text(option)
+                        Image(systemName: "heart.fill")
+                    }
+                    .tag(option)
+
+                }
+            })
+        .pickerStyle(MenuPickerStyle())
+        
+        /* WheelPickerStyle */
+        /*
         VStack {
             HStack {
                 Text("Age:")
@@ -30,7 +63,7 @@ struct Basics_26_Picker_Bootcamp: View {
                        })
                        .pickerStyle(WheelPickerStyle())
             })
-        }
+        } */
     }
 }
 
